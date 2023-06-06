9986a5088430090b166fd99cce102ca6d9fe4e96
Askar Karasaev <askar.karasaev@gmail.com>
2023-06-06 22:56:54 +0900

    adding 3 collections update

---
+++ src/api/prefecture/routes/prefecture.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * prefecture router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::prefecture.prefecture');
---
+++ src/api/prefecture/services/prefecture.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * prefecture service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::prefecture.prefecture');
